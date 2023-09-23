32ecc065fd73874b44208ecbad44e1d0e48a2708
Jaskaran-0 <jaskaran-singh-malhotra@mohawkcollege.ca>
2023-09-23 11:37:32 -0400

    initializing react app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { Typography } from '@mui/material';
+
+function App() {
+    return (
+        <>
+            <Typography>Hello Wrold</Typography>
+        </>
+    );
+}
+
+export default App;
