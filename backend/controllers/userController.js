96129fc100758541080be618afa2875a6f199822
Jaskaran-0 <jaskaran-singh-malhotra@mohawkcollege.ca>
2023-09-24 12:26:23 -0400

    fixed bug

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,46 +1,46 @@
-const User = require("../models/userModel");
-const generateToken = require("../config/generateToken");
-const expressAsyncHandler = require("express-async-handler");
-const NodeRSA = require("node-rsa");
-const transporter = require ('../config/transporter');
+const User = require('../models/userModel');
+const generateToken = require('../config/generateToken');
+const expressAsyncHandler = require('express-async-handler');
+const NodeRSA = require('node-rsa');
+const transporter = require('../config/transporter');
 const jwt = require('jsonwebtoken');
 
 const registerUser = expressAsyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+    const { name, email, password } = req.body;
 
-  if (!name || !email || !password) {
-    res.status(400);
-    throw new Error("Please enter all the fields");
-  }
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error('Please enter all the fields');
+    }
 
-  const userExists = await User.findOne({ email });
-  if (userExists) {
-    res.status(403);
-    throw new Error("User already exists");
-  }
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(403);
+        throw new Error('User already exists');
+    }
 
-  const key = new NodeRSA({ b: 2048 }); 
+    const key = new NodeRSA({ b: 2048 });
 
-  // Extract private and public keys as strings
-  const privateKey = key.exportKey("private");
-  const publicKey = key.exportKey("public");
+    // Extract private and public keys as strings
+    const privateKey = key.exportKey('private');
+    const publicKey = key.exportKey('public');
 
-  const user = await User.create({
-    name,
-    email,
-    password,
-    privateKey,
-    publicKey,
-  });
+    const user = await User.create({
+        name,
+        email,
+        password,
+        privateKey,
+        publicKey,
+    });
 
-  if (user) {
-    res.send("User created succesfully");
-    const token = generateToken(user._id);
-    const mailOptions = {
-      from: process.env.APP_EMAIL,
-      to: email,
-      subject: "Email Verification",
-      html: `
+    if (user) {
+        res.send('User created succesfully');
+        const token = generateToken(user._id);
+        const mailOptions = {
+            from: process.env.APP_EMAIL,
+            to: email,
+            subject: 'Email Verification',
+            html: `
           <div>Click the following link to verify your email</div>
           <div>
               <a href=${process.env.SERVER_URI}/user/verify/${token}>
@@ -48,71 +48,68 @@ const registerUser = expressAsyncHandler(async (req, res) => {
               </a>
           </div>
           `,
-    };
+        };
 
-    await transporter.sendMail(mailOptions);
-  } else {
-    res.status(400);
-    throw new Error("Failed to create user");
-  }
+        await transporter.sendMail(mailOptions);
+    } else {
+        res.status(400);
+        throw new Error('Failed to create user');
+    }
 });
 
 const verifyUser = expressAsyncHandler(async (req, res) => {
-  const token = req.params.token;
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.id);
-    user.isVerified = true;
-    await user.save();
-    res.status(200).send("succesfull")
-  } catch (err) {
-    console.log(err);
-    res.status(500).redirect("failed to validate email");
-  }
+    const token = req.params.token;
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.id);
+        user.isVerified = true;
+        await user.save();
+        res.status(200).send('succesfull');
+    } catch (err) {
+        console.log(err);
+        res.status(500).redirect('failed to validate email');
+    }
 });
 
 const authUser = expressAsyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-  if (!user) {
-    res.status(404);
-    throw new Error("Email is not associated with any account");
-  }
-
-  if (await user.matchPassword(password)) {
-    if(!user.isVerified) {
-      res.status(401);
-        throw new Error("User's email is not verified")
+    if (!user) {
+        res.status(404);
+        throw new Error('Email is not associated with any account');
     }
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id)
-    });
-  } else {
-    res.status(401);
-    throw new Error("Invalid Email or Password");
-  }
+
+    if (await user.matchPassword(password)) {
+        if (!user.isVerified) {
+            res.status(401);
+            throw new Error("User's email is not verified");
+        }
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid Email or Password');
+    }
 });
 
 const allUsers = expressAsyncHandler(async (req, res) => {
-  const keyword = req.query.search
-    ? {
-        $or: [
-          { name: { $regex: req.query.search, $options: "i" } },
-          { email: { $regex: req.query.search, $options: "i" } },
-        ],
-      }
-    : {};
+    // console.log(req.query.search,"here");
+    const keyword = req.query.search
+        ? {
+              email: { $regex: req.query.search, $options: 'i' },
+          }
+        : {};
 
-  const users = await User.find(keyword)
-    .find({ _id: { $ne: req.user._id } })
-    .select("-password -privateKey");
+    const users = await User.find(keyword)
+        .find({ _id: { $ne: req.user._id } })
+        .select('-password -privateKey');
 
-  res.json(users);
+    res.json(users);
 });
 
 module.exports = { registerUser, authUser, allUsers, verifyUser };
-
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const {protect} = require('../middleware/authMiddleware')
+const { protect } = require('../middleware/authMiddleware');
 
-const {registerUser, authUser, allUsers,verifyUser} = require('../controllers/userController')
+const {
+    registerUser,
+    authUser,
+    allUsers,
+    verifyUser,
+} = require('../controllers/userController');
 
-router.post('/register', registerUser)
+router
+    .post('/register', registerUser)
     .post('/login', authUser)
-    .get('/:search',protect, allUsers)
+    .get('/', protect, allUsers)
     .get('/verify/:token', verifyUser);
-
 
 module.exports = router;
